4f91497de15a3c29d2d6c3a523ef60e447800821
kccnma <gargiulo@hawaii.edu>
2024-10-07 16:36:14 -1000

    completed simple base - final steps

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 // TOGGLE MENU
 const toggleMenu = document.querySelector('.toggle-menu');
 const toggleMenuButton = document.querySelector('.site-menu-button');
+const toggleMenuLinks = document.querySelectorAll('.toggle-menu a');
 toggleMenuButton.onclick = function() {
     if (toggleMenu.getAttribute('data-menustate') === 'closed') {
       // if closed, open it    
@@ -11,3 +12,9 @@ toggleMenuButton.onclick = function() {
       toggleMenu.setAttribute('data-menustate','closed');
     }
 };
+// CLOSE THE MENU WHEN A USER CLICKS ON ANY LINK
+toggleMenuLinks.forEach((el) => {
+  el.onclick = () => {
+    toggleMenu.setAttribute('data-menustate','closed');
+  }
+});
