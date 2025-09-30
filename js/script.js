8e370fd95369ef4af9f92e58c4b76c1a1872439e
kccnma <gargiulo@hawaii.edu>
2025-09-30 13:19:28 -1000

    added menu button, halfway through navicon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
-    // GLOBAL FUNCTIONS GO HERE
+    // ENABLE MOBILE MENU 
+    const sitenav = document.querySelector(".site-nav");
+    const menubutton = document.querySelector(".menubutton");
+
+    menubutton.onclick = () => {
+    
+        // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
+        if (sitenav.getAttribute("data-menustate") === "closed" ) {
+            sitenav.setAttribute("data-menustate", "open");
+        } else {
+            sitenav.setAttribute("data-menustate", "closed");
+        };
+    
+    };
 
 
 });
