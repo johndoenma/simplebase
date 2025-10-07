29cf477a63fe2e0cf04c3a94d2e19f13ffed5502
kccnma <gargiulo@hawaii.edu>
2025-10-07 11:33:16 -1000

    fixed drawer nav animation bug

--- js/script.js
+++ js/script.js
@@ -5,15 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const menubutton = document.querySelector(".menubutton");
 
     menubutton.onclick = () => {
-    
         // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
         if (sitenav.getAttribute("data-menustate") === "open" ) {
             sitenav.setAttribute("data-menustate", "closed");
         } else {
             sitenav.setAttribute("data-menustate", "open");
         };
-    
     };
 
+    // Remove the data-attribute on resize to fix drawer nav animation
+    window.onresize = () => {
+        sitenav.removeAttribute("data-menustate");
+    };
 
 });
+
